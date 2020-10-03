68302c250ad6d8c0093578b369e476d0875166b9
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-03 21:36:44 +0300

    feat: add route for info page

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Info from './Info';
 import Jogs from './Jogs';
 import Login from './Login';
 
@@ -7,8 +8,9 @@ class Routes extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/jogs" component={Jogs} />
-        <Route path="*" component={Login} />
+        <Route exact path="/info" component={Info} />
+        <Route exact path="/jogs" component={Jogs} />
+        <Route path="*/" component={Login} />
       </Switch>
     );
   }
