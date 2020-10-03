ea99e03071c86910d0a021267ba80ce379fee8b2
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-03 22:49:32 +0300

    feat: add route for new jog form

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Info from './Info';
+import JogForm from './JogForm';
 import Jogs from './Jogs';
 import Login from './Login';
 
@@ -8,6 +9,7 @@ class Routes extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path="/new-jog" component={JogForm} />
         <Route exact path="/info" component={Info} />
         <Route exact path="/jogs" component={Jogs} />
         <Route path="*/" component={Login} />
