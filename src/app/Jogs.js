e965af0987b5924b45c2b08a260637ab3e2c847b
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-03 17:20:02 +0300

    feat: add jogs page component

---
+++ src/app/Jogs.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+const jogs = [
+  {
+    distance: '10',
+    time: '60',
+    date: '20.12.2017',
+    speed: '15',
+  },
+  {
+    distance: '10',
+    time: '60',
+    date: '20.12.2017',
+    speed: '15',
+  },
+  {
+    distance: '10',
+    time: '60',
+    date: '20.12.2017',
+    speed: '15',
+  },
+  {
+    distance: '10',
+    time: '60',
+    date: '20.12.2017',
+    speed: '15',
+  },
+];
+
+export default class Jogs extends Component {
+  render() {
+    return (
+      <>
+        <div className="date_filters">
+          <div className="date_filters__wrapper">
+            <label forHtml="from">Date from </label>
+            <input className="date_filters__input" id="from" />
+          </div>
+
+          <div className="date_filters__wrapper">
+            <label forHtml="to">Date to</label>
+            <input className="date_filters__input" id="to" />
+          </div>
+        </div>
+        <div className="jogs">
+          {jogs.map(jog => (
+            <Jog jog={jog} />
+          ))}
+        </div>
+      </>
+    );
+  }
+}
+
+const Jog = props => {
+  const { speed, distance, time, date } = props.jog;
+  return (
+    <div className="jogs__jog">
+      <p>{date}</p>
+      <p>
+        <span className="jogs__important_text">Speed:</span> {speed}
+      </p>
+      <p>
+        <span className="jogs__important_text">Distance</span> {distance} km
+      </p>
+      <p>
+        <span className="jogs__important_text">Time</span> {time} min
+      </p>
+    </div>
+  );
+};
