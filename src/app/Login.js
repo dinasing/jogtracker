ef0b1726f794b66d1e9d974eb7103c287a230008
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-03 15:22:44 +0300

    feat: add login component

---
+++ src/app/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="login">
+          <button className="login__button">Let me in</button>
+        </div>
+      </div>
+    );
+  }
+}
--- src/app/Routes.js
+++ src/app/Routes.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Login from './Login';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
+        <Route path="*" component={Login} />
         <Route path="*" component={() => '404 NOT FOUND'} />
       </Switch>
     );
