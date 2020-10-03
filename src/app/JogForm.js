9fd0183e7fc5196075e4466b6f9cef7ac0bbd13e
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-04 01:21:54 +0300

    feat: make cancel icon link to jogs page

--- src/app/JogForm.js
+++ src/app/JogForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import cancelIcon from '../../assets/images/cancel.svg';
 
 export default class JogForm extends Component {
@@ -6,7 +7,9 @@ export default class JogForm extends Component {
     return (
       <div className="modal">
         <div className="modal__cancel">
-          <img src={cancelIcon} alt="cancel" />
+          <Link to="jogs">
+            <img src={cancelIcon} alt="cancel" />
+          </Link>
         </div>
 
         <form className="form">
