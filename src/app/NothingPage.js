8fa1357068fddb409c4a911fe651d8715b546d54
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-03 23:54:10 +0300

    feat: create component file

---
+++ src/app/NothingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// add this component to jogs list page with check for jogs
+export default function NothingPage() {
+  return (
+    <div>
+      <p> Nothing is there</p>
+      <button>Create your first jog</button>
+    </div>
+  );
+}
