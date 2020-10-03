aaa1fe0820b639effee11f3acddb1e08b02150d3
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-04 00:59:14 +0300

    feat: add route for nothing there page

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -4,6 +4,7 @@ import Info from './Info';
 import JogForm from './JogForm';
 import Jogs from './Jogs';
 import Login from './Login';
+import NothingPage from './NothingPage';
 
 class Routes extends Component {
   render() {
@@ -12,7 +13,8 @@ class Routes extends Component {
         <Route exact path="/new-jog" component={JogForm} />
         <Route exact path="/info" component={Info} />
         <Route exact path="/jogs" component={Jogs} />
-        <Route path="*/" component={Login} />
+        <Route path="/login" component={Login} />
+        <Route path="*" component={NothingPage} />
       </Switch>
     );
   }
