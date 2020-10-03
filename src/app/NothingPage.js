aec7d35458141e42e8b41d1534c093f68b9a759a
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-04 01:24:55 +0300

    feat: make button on nothing page link to new jog form

--- src/app/NothingPage.js
+++ src/app/NothingPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NothingPage() {
   return (
     <div className="nothing_page">
-      <p className="nothing_page__text"> Nothing is there</p>
-      <button className="nothing_page__button" type="button">
-        Create your jog first
-      </button>
+      <p className="nothing_page__text"> Nothing is there</p>{' '}
+      <Link to="/new-jog">
+        <button className="nothing_page__button" type="button">
+          Create your jog first
+        </button>
+      </Link>
     </div>
   );
 }
