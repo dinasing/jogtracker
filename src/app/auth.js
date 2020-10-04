6a38882f8a5de5a0c9f8c42a7e29ffd895c5c6ec
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-04 16:51:07 +0300

    feat: implement auth with token

---
+++ src/app/auth.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+export const tokenConfig = config => {
+  const token = localStorage.getItem('token');
+
+  config.headers = {
+    'Content-type': 'application/json',
+  };
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+};
+
+axios.interceptors.request.use(tokenConfig);
+
+export const getToken = uuid => {
+  const config = {};
+  config.headers = {
+    'Content-type': 'application/x-www-form-urlencoded',
+  };
+  const data = { uuid };
+
+  axios
+    .post('https://jogtracker.herokuapp.com/api/v1/auth/uuidLogin', config, { data })
+    .then(response => {
+      if (response.data.response.access_token) {
+        localStorage.setItem('token', response.data.response.access_token);
+      }
+    })
+    .catch(() => {
+      alert('Oops! Something went wrong...');
+    });
+};
+
+export const isTokenValid = async () => {
+  axios.get('https://jogtracker.herokuapp.com/api/v1/auth/user').catch(() => {
+    localStorage.removeItem('token');
+    return false;
+  });
+
+  return true;
+};
