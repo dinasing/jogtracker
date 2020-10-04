006117723db9ab985b40f1dda480274a02411532
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-04 17:17:53 +0300

    feat: change dummy data to real jogs

--- src/app/Jogs.js
+++ src/app/Jogs.js
@@ -1,46 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 import addIcon from '../../assets/images/addicon.svg';
-
-const jogs = [
-  {
-    distance: '10',
-    time: '60',
-    date: '20.12.2017',
-    speed: '15',
-  },
-  {
-    distance: '10',
-    time: '60',
-    date: '20.12.2017',
-    speed: '15',
-  },
-  {
-    distance: '10',
-    time: '60',
-    date: '20.12.2017',
-    speed: '15',
-  },
-  {
-    distance: '10',
-    time: '60',
-    date: '20.12.2017',
-    speed: '15',
-  },
-];
+import { loadJogs } from './jogs.actions';
+import NothingPage from './NothingPage';
 
 export default class Jogs extends Component {
+  state = {
+    jogs: [],
+  };
+
+  async componentDidMount() {
+    const jogs = await loadJogs();
+    this.setState({ jogs });
+  }
+
   render() {
+    const { jogs } = this.state;
+    if (!jogs.length) {
+      return <NothingPage />;
+    }
     return (
       <>
         <div className="date_filters">
           <div className="date_filters__wrapper">
-            <label forHtml="from">Date from </label>
+            <label>Date from </label>
             <input className="date_filters__input" id="from" />
           </div>
 
           <div className="date_filters__wrapper">
-            <label forHtml="to">Date to</label>
+            <label>Date to</label>
             <input className="date_filters__input" id="to" />
           </div>
         </div>
@@ -60,12 +49,13 @@ export default class Jogs extends Component {
 }
 
 const Jog = props => {
-  const { speed, distance, time, date } = props.jog;
+  const { distance, time, date } = props.jog;
+  const speed = time ? distance / time : 0;
   return (
     <article className="jogs__jog">
-      <p className="jogs__date jogs__jog_info_text">{date}</p>
+      <p className="jogs__date jogs__jog_info_text">{moment(date).format('DD.MM.YYYY')}</p>
       <p className="jogs__jog_info_text">
-        <span className="jogs__important_text ">Speed:</span> {speed}
+        <span className="jogs__important_text ">Speed:</span> {speed.toPrecision(2)}
       </p>
       <p className="jogs__jog_info_text">
         <span className="jogs__important_text">Distance:</span> {distance} km
