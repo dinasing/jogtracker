d03c46d5fbc77f276b639db1ff738b920aaae055
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-05 01:12:47 +0300

    feat: add handlers for inputs value changes and submit

--- src/app/JogForm.js
+++ src/app/JogForm.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import cancelIcon from '../../assets/images/cancel.svg';
+import { postJog } from './jogs.actions';
 
 export default class JogForm extends Component {
+  state = {
+    date: '',
+    time: '',
+    distance: '',
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { date, time, distance } = this.state;
+    const jog = { date, time, distance };
+    postJog(jog);
+  };
+
   render() {
     return (
       <div className="modal">
@@ -15,17 +36,32 @@ export default class JogForm extends Component {
         <form className="form">
           <p className="input_group">
             <label className="input_group__label">Distance</label>
-            <input className="input_group__input" type="text" />
+            <input
+              className="input_group__input"
+              type="text"
+              id="distance"
+              onChange={this.handleChange}
+            />
           </p>
           <p className="input_group">
             <label className="input_group__label">Time</label>
-            <input className="input_group__input" type="time" />
+            <input
+              className="input_group__input"
+              type="time"
+              id="time"
+              onChange={this.handleChange}
+            />
           </p>
           <p className="input_group">
             <label className="input_group__label">Date</label>
-            <input className="input_group__input" type="date" />
+            <input
+              className="input_group__input"
+              type="date"
+              id="date"
+              onChange={this.handleChange}
+            />
           </p>
-          <button className="form__submit" type="submit">
+          <button className="form__submit" type="submit" onClick={this.handleSubmit}>
             Save
           </button>
         </form>
