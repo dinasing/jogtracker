342e6758d7e65bf1190af5373bc76601df00cf2c
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-04 16:51:33 +0300

    feat: add check for token

--- src/app/App.js
+++ src/app/App.js
@@ -4,12 +4,32 @@ import Routes from './Routes';
 import '../scss/main.scss';
 import NavBar from './NavBar';
 
+import { isTokenValid } from './auth';
+
 class App extends React.Component {
+  state = {
+    isAuthenticated: false,
+  };
+
+  componentDidMount() {
+    this.checkToken();
+  }
+
+  checkToken = async () => {
+    const token = localStorage.getItem('token');
+    let isAuthenticated = false;
+    if (token) {
+      isAuthenticated = await isTokenValid();
+    }
+    this.setState({ isAuthenticated });
+  };
+
   render() {
+    const { isAuthenticated } = this.state;
     return (
       <BrowserRouter>
-        <NavBar />
-        <Routes />
+        <NavBar isAuthenticated={isAuthenticated} />
+        <Routes isAuthenticated={isAuthenticated} />
       </BrowserRouter>
     );
   }
