ee9ef1f6609d8998d8f84e76a5b31eb902a7c654
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-04 16:54:04 +0300

    feat: make button request for token

--- src/app/Login.js
+++ src/app/Login.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { getToken } from './auth';
+
+const { uuid } = process.env;
 
 export default class Login extends Component {
+  handleLogin = () => {
+    getToken(uuid);
+  };
+
   render() {
     return (
       <div className="wrapper">
         <div className="login">
-          <button className="login__button">Let me in</button>
+          <button onClick={this.handleLogin} className="login__button">
+            Let me in
+          </button>
         </div>
       </div>
     );
