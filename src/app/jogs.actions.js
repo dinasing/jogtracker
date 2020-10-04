d9a29d6965cd0d208bd4a42fc917e6ae47ed4e14
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-05 01:21:17 +0300

    feat: add function to post a jog

--- src/app/jogs.actions.js
+++ src/app/jogs.actions.js
@@ -9,3 +9,20 @@ export const loadJogs = async () => {
 
   return response.data.response.jogs;
 };
+
+export const postJog = jog => {
+  const config = {};
+  config.headers = {
+    'Content-type': 'application/x-www-form-urlencoded',
+  };
+  const data = { ...jog };
+
+  axios
+    .post('https://jogtracker.herokuapp.com/api/v1/data/jog', config, { data })
+    .then(response => {
+      alert('Jog has been added successfully!');
+    })
+    .catch(error => {
+      alert('Jog has not been added!');
+    });
+};
