2bc1e7180260a666d886515b952e33fa108e4ef8
Dina Singatulina <dina.sing1212@gmail.com>
2020-10-06 16:10:52 +0300

    feat: add classes for mobile nav

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -3,13 +3,26 @@ import { NavLink, Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.svg';
 import filter from '../../assets/images/filter.svg';
 import filterActive from '../../assets/images/filter-active.svg';
+import menuIcon from '../../assets/images/menu.png';
+import cancelIcon from '../../assets/images/cancel-menu.svg';
 
 export default class NavBar extends Component {
+  state = {
+    isMenuOpen: false,
+  };
+
+  toggleMenu = () => {
+    this.setState(prevState => ({
+      isMenuOpen: !prevState.isMenuOpen,
+    }));
+  };
+
   render() {
     const { isAuthenticated, areFiltersOpen } = this.props;
+    const { isMenuOpen } = this.state;
 
     return (
-      <header className="navbar">
+      <header className={isMenuOpen ? 'navbar navbar-open' : 'navbar'}>
         <Link className="navbar__home_link" to="/">
           <img className="navbar__logo" src={logo} alt="logobear" />
         </Link>
@@ -39,6 +52,14 @@ export default class NavBar extends Component {
                 type="image"
                 onClick={this.props.toggleFilters}
                 src={areFiltersOpen ? filterActive : filter}
+              />
+            </li>
+            <li className="navbar__menu_icon">
+              <input
+                type="image"
+                onClick={this.toggleMenu}
+                src={isMenuOpen ? cancelIcon : menuIcon}
+                width="28px"
               />
             </li>
           </ul>
